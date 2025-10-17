2c45bff318044d4019ccb44fc3bb4ef5474c4e07
zx-in-offi <ziyasheikh2005@gmail.com>
2025-10-17 10:44:10 +0530

    commit

--- pro-portfolio/vite.config.js
+++ pro-portfolio/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Portfolio/', // Important for GitHub Pages
   plugins: [react()],
-})
+});
